bdbc0cb18f869cfd7f8b2579bbbe82a8d3dbfb04
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2026-01-04 23:54:58 +0630

    update

---
+++ oop/src/oop/Car.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class Car {
+
+
+		String name = "Rav4";
+		String model = "Toyota";
+		boolean start = false;
+		
+	
+	
+	void startEngine() {
+		System.out.println("Start Engine");
+	}
+
+}
---
+++ oop/src/oop/Main.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Car car = new Car();	
+		System.out.println("My car is " + car.name);
+		car.startEngine();
+	}
+
+}
